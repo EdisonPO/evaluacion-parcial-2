fcc6358b793ad24fb94b8d5ac0de8473932f8eaa
EdisonPO <edisondavid.p90@gmail.com>
2026-07-24 18:57:54 -0500

    feature: Ejercicio 2: variables y tipos de datos

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let nombreProducto1 = "Celular Nokia";
+let precioProducto1 = 350;
+let disponibleProducto1 = true;
+
+let nombreProducto2 = "Mouse inalambrico";
+let precioProducto2 = 20 ;
+let disponibleProducto2 = false;
+
+console.log(`Producto: ${nombreProducto1}, Precio: ${precioProducto1}, Disponible: ${disponibleProducto1}`)
+console.log(`Producto: ${nombreProducto2}, Precio: ${precioProducto2}, Disponible: ${disponibleProducto2}`);
